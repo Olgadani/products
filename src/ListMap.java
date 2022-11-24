cd7b2b83f431bb65b12cec55612de835b1ceb25d
Olgadani <olga.metizzz@yandex.ru>
2022-11-24 22:01:05 +0400

    homework

---
+++ src/ListMap.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ListMap {
+    private final static Random RANDOM = new Random();
+
+    private final Map<String, List<Integer>> listMap = new HashMap<>();
+
+    public ListMap() {
+        for (int i = 0; i < 5; i++) {
+            listMap.put(String.valueOf(i), generateList());
+        }
+    }
+
+    public Set<Map.Entry<String, List<Integer>>> getMapEntries() {
+        return listMap.entrySet();
+    }
+
+    public Map<String, Integer> getTransformedCollection() {
+        Map<String, Integer> result = new HashMap<>();
+        for (Map.Entry<String, List<Integer>> entry : listMap.entrySet()) {
+            int sum = 0;
+            for (Integer number : entry.getValue()) {
+                sum += number;
+            }
+            result.put(entry.getKey(), sum);
+        }
+        return result;
+    }
+
+    private List<Integer> generateList(){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            list.add(RANDOM.nextInt(1001));
+        }
+        return list;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,21 @@ public class Main {
         for (Map.Entry<FullName, String> entry : phoneGuide.getEntries()) {
             System.out.printf("%s - %s%n", entry.getKey(), entry.getValue());
         }
+        System.out.println("Task 2.2");
+        Map<Integer, String> orderedMap = new LinkedHashMap<>();
+        orderedMap.put(1, "test1");
+        orderedMap.put(2, "test2");
+        orderedMap.put(3, "test3");
+        orderedMap.put(4, "test4");
+        orderedMap.put(5, "test5");
+        orderedMap.put(6, "test6");
+        orderedMap.put(7, "test7");
+        orderedMap.put(8, "test8");
+        orderedMap.put(9, "test9");
+        orderedMap.put(10, "test10");
+        for (Map.Entry<Integer, String> entry : orderedMap.entrySet()) {
+            System.out.printf("%d : %s%n", entry.getKey(), entry.getValue());
+        }
     }
 
     private static void numbersRemove() {
